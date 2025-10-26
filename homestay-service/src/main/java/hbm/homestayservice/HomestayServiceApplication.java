ac27829e6095843478b6ef6d0ed775dc39de3042
gnouh404 <090297huong@gmail.com>
2025-10-26 14:48:00 +0700

    first commit

---
+++ homestay-service/src/main/java/hbm/homestayservice/HomestayServiceApplication.java
@@ -0,0 +1,13 @@
+package hbm.homestayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomestayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomestayServiceApplication.class, args);
+    }
+
+}
---
+++ homestay-service/src/test/java/hbm/homestayservice/HomestayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hbm.homestayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomestayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
