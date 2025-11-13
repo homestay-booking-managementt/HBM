44612804ae618a671c8bc5a0af7725039e81b686
gnouh404 <nconghuong1401@gmail.com>
2025-11-13 15:35:19 +0700

    add websocket chat feat

---
+++ chat-service/src/main/java/hbm/chatservice/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package hbm.chatservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // Client sẽ subscribe topic /topic/session.{sessionId}
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-chat") // endpoint để client connect
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); // fallback nếu trình duyệt ko support WS
+    }
+}
