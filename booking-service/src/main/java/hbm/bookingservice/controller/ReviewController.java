0f6740010257ea8a70614f6e7c288bd30087d43e
gnouh404 <nconghuong1401@gmail.com>
2025-11-14 12:51:43 +0700

    add cross origin for review

--- booking-service/src/main/java/hbm/bookingservice/controller/ReviewController.java
+++ booking-service/src/main/java/hbm/bookingservice/controller/ReviewController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/reviews")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3200")
 public class ReviewController {
 
     private final ReviewService reviewService;
